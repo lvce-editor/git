6953f79f8798d260933e424e9c16a0b4fc0787e6
Le Vivilet <levivlet@pm.me>
2022-09-07 12:21:02 +0200

    extract git push error server test executable

--- packages/e2e/src/git.push-error-server.js
+++ packages/e2e/src/git.push-error-server.js
@@ -2,24 +2,14 @@ test('git.push-error-server', async () => {
   // arrange
   const tmpDir = await FileSystem.getTmpDir({ scheme: 'file' })
   await Workspace.setPath(tmpDir)
-  const gitPath = await FileSystem.createExecutable(`
-console.info(\`Enumerating objects: 23, done.
-Counting objects: 100% (23/23), done.
-Delta compression using up to 8 threads
-Compressing objects: 100% (13/13), done.
-Writing objects: 100% (13/13), 5.33 KiB | 780.00 KiB/s, done.
-Total 13 (delta 8), reused 0 (delta 0), pack-reused 0
-remote: Resolving deltas: 100% (8/8), completed with 8 local objects.\`)
-console.error(\`remote: Internal Server Error
-To github.com:user/repo.git
- ! [remote rejected]           main -> main (Internal Server Error)
-error: failed to push some refs to 'github.com:user/repo.git\`)
-process.exit(128)
-`)
+  const gitPath = await FileSystem.createExecutableFrom(
+    `fixtures/git.push-error-server/git.js`
+  )
   await Settings.update({
     'git.path': gitPath,
   })
 
+  // TODO have a faster way to execute commands as user, no need to open quick pick
   // act
   await QuickPick.open()
   await QuickPick.setValue('>Git: Push')
