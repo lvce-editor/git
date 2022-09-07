017800fab7a6325f483d395046b63b7e5189b20d
Le Vivilet <levivlet@pm.me>
2022-09-07 12:18:29 +0200

    extract git pull error email privacy test executable

---
+++ packages/e2e/fixtures/git.push-error-email-privacy-restrictions/git.js
@@ -0,0 +1,12 @@
+console.info(`Enumerating objects: 23, done.
+Counting objects: 100% (23/23), done.
+Delta compression using up to 8 threads
+Compressing objects: 100% (13/13), done.
+Writing objects: 100% (13/13), 5.33 KiB | 780.00 KiB/s, done.
+Total 13 (delta 8), reused 0 (delta 0), pack-reused 0
+remote: Resolving deltas: 100% (8/8), completed with 8 local objects.\`)
+console.error(\`remote: error: GH007: Your push would publish a private email address.
+To github.com:user/repo.git
+ ! [remote rejected] master -> master (push declined due to email privacy restrictions)
+error: failed to push some refs to 'github.com:user/repo.git`)
+process.exit(128)
