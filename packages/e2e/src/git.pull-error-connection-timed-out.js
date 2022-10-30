8d1fc9355ebb3394ab7c3d5dd1ba64e0fdb1a6d4
Le Vivilet <levivlet@pm.me>
2022-10-30 17:25:58 +0100

    add e2e test for git connection timeout

---
+++ packages/e2e/src/git.pull-error-connection-timed-out.js
@@ -0,0 +1,24 @@
+test('git.pull-error-connection-timed-out', async () => {
+  const tmpDir = await FileSystem.getTmpDir({ scheme: 'file' })
+  await Workspace.setPath(tmpDir)
+  const gitPath = await FileSystem.createExecutableFrom(
+    `fixtures/git.pull-error-connection-timed-out/git.js`
+  )
+  await Settings.update({
+    'git.path': gitPath,
+  })
+
+  // act
+  await QuickPick.open()
+  await QuickPick.setValue('>Git: Pull')
+  await QuickPick.selectItem('Git: Pull')
+  // assert
+  const dialogErrorMessage = Locator('#DialogBodyErrorMessage')
+  await expect(dialogErrorMessage).toBeVisible()
+  // TODO error message could be improved, should include full git error message
+  await expect(dialogErrorMessage).toHaveText(
+    'Error: Git: ssh: connect to host github.com port 22: Connection timed out'
+  )
+})
+
+export {}
