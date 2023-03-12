d2ddc1a40b2e1f6c382392540744fe371bb6b5b9
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2023-03-12 19:15:18 +0100

    refactor: move getChangedFiles function into separate module (#38)

---
+++ packages/extension/src/parts/GetChangedFiles/GetChangedFiles.js
@@ -0,0 +1,35 @@
+import * as GetDecorationIcon from '../GetDecorationIcon/GetDecorationIcon.js'
+import * as GetDecorationStrikeThrough from '../GetDecorationStrikeThrough/GetDecorationStrikeThrough.js'
+import * as GetStatusText from '../GetStatusText/GetStatusText.js'
+import * as Repositories from '../GitRepositories/GitRepositories.js'
+import * as GitRequests from '../GitRequests/GitRequests.js'
+
+export const id = 'git'
+
+export const label = 'Git'
+
+const getWithDecoration = (resource) => {
+  return {
+    ...resource,
+    icon: GetDecorationIcon.getDecorationIcon(resource.status),
+    iconTitle: GetStatusText.getStatusText(resource.status),
+    strikeThrough: GetDecorationStrikeThrough.getDecorationStrikeThrough(
+      resource.status
+    ),
+  }
+}
+
+const getWithDecorations = (index) => {
+  return index.map(getWithDecoration)
+}
+
+export const getChangedFiles = async (cwd) => {
+  const repository = await Repositories.getCurrent()
+  const modifiedFiles = await GitRequests.getModifiedFiles({
+    cwd: repository.path,
+    gitPath: repository.gitPath,
+  })
+  const { index } = modifiedFiles
+  const indexWithDecorations = getWithDecorations(index)
+  return indexWithDecorations
+}
