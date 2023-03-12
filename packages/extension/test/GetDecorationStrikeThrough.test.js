0b6ebb22e00f3fbd2464ea5ab4a17d56aaa3d3ef
Le Vivilet <levivlet@pm.me>
2023-03-12 11:05:19 +0100

    feature: add strike-through decoration

---
+++ packages/extension/test/GetDecorationStrikeThrough.test.js
@@ -0,0 +1,18 @@
+import * as GetDecorationStrikeThrough from '../src/parts/GetDecorationStrikeThrough/GetDecorationStrikeThrough.js'
+import * as FileStateType from '../src/parts/FileStateType/FileStateType.js'
+
+test('getDecorationStrikeThrough - both deleted', () => {
+  expect(
+    GetDecorationStrikeThrough.getDecorationStrikeThrough(
+      FileStateType.BothDeleted
+    )
+  ).toBe(true)
+})
+
+test('getDecorationStrikeThrough - index added', () => {
+  expect(
+    GetDecorationStrikeThrough.getDecorationStrikeThrough(
+      FileStateType.IndexAdded
+    )
+  ).toBe(false)
+})
