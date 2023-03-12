6c7e9d96b6f02db071aaa0d512c34f2703b3d2aa
Le Vivilet <levivlet@pm.me>
2023-03-12 19:13:55 +0100

    refactor: remove unused code

--- packages/extension/src/parts/Exec/Exec.js
+++ packages/extension/src/parts/Exec/Exec.js
@@ -1,30 +1,5 @@
 import * as Trace from '../Trace/Trace.js'
 
-export const state = {
-  /**
-   * @param {string} command
-   * @param {string[]} args
-   * @param {import('execa').Options} options
-   * @returns
-   */
-  async exec(command, args, options) {
-    const start = performance.now()
-    const { stdout, stderr } = await vscode.exec(command, args, {
-      ...options,
-    })
-    const end = performance.now()
-    Trace.trace(
-      `git ${args.join(' ')} (${(end - start).toFixed(
-        1
-      )}ms)\n${stdout}\n${stderr}`
-    )
-    return {
-      stdout,
-      stderr,
-    }
-  },
-}
-
 /**
  *
  * @param {string} command
@@ -32,8 +7,21 @@ export const state = {
  * @param {import('execa').Options} options
  * @returns
  */
-export const exec = (command, args, options) => {
-  return state.exec(command, args, options)
+export const exec = async (command, args, options) => {
+  const start = performance.now()
+  const { stdout, stderr } = await vscode.exec(command, args, {
+    ...options,
+  })
+  const end = performance.now()
+  Trace.trace(
+    `git ${args.join(' ')} (${(end - start).toFixed(
+      1
+    )}ms)\n${stdout}\n${stderr}`
+  )
+  return {
+    stdout,
+    stderr,
+  }
 }
 
 export const isExecError = (error) => {
