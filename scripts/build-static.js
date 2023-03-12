59a03fa52136ea9cd54734e11e72eadba60fea23
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2023-03-12 19:42:25 +0100

    include git worker in build (#41)

--- scripts/build-static.js
+++ scripts/build-static.js
@@ -1,4 +1,5 @@
 import { exportStatic } from '@lvce-editor/shared-process'
+import { cp, readdir } from 'node:fs/promises'
 import path, { dirname } from 'node:path'
 import { fileURLToPath } from 'node:url'
 
@@ -10,3 +11,27 @@ await exportStatic({
   testPath: 'packages/e2e',
   root,
 })
+
+const RE_COMMIT_HASH = /^[a-z\d]+$/
+const isCommitHash = (dirent) => {
+  return dirent.length === 7 && dirent.match(RE_COMMIT_HASH)
+}
+
+const dirents = await readdir(path.join(root, 'dist'))
+const commitHash = dirents.find(isCommitHash) || ''
+
+for (const dirent of ['src']) {
+  await cp(
+    path.join(root, 'packages', 'git-worker', dirent),
+    path.join(
+      root,
+      'dist',
+      commitHash,
+      'extensions',
+      'builtin.git',
+      'git-worker',
+      dirent
+    ),
+    { recursive: true, force: true }
+  )
+}
--- scripts/build.js
+++ scripts/build.js
@@ -1,47 +1,12 @@
-import { execSync } from 'child_process'
+import { packageExtension } from '@lvce-editor/package-extension'
 import fs, { readFileSync } from 'fs'
 import path, { dirname, join } from 'path'
 import { fileURLToPath } from 'url'
-import { packageExtension } from '@lvce-editor/package-extension'
-
-const NOT_NEEDED = [
-  'node_modules/typescript/bin',
-  'node_modules/typescript/loc',
-  'node_modules/typescript/AUTHORS.md',
-  'node_modules/typescript/CODE_OF_CONDUCT.md',
-  'node_modules/typescript/CopyrightNotice.txt',
-  'node_modules/typescript/LICENSE.txt',
-  'node_modules/typescript/README.md',
-  'node_modules/typescript/SECURITY.md',
-  'node_modules/typescript/ThirdPartyNoticeText.txt',
-  'node_modules/typescript/lib/cs',
-  'node_modules/typescript/lib/de',
-  'node_modules/typescript/lib/es',
-  'node_modules/typescript/lib/fr',
-  'node_modules/typescript/lib/it',
-  'node_modules/typescript/lib/ja',
-  'node_modules/typescript/lib/ko',
-  'node_modules/typescript/lib/pl',
-  'node_modules/typescript/lib/pt-br',
-  'node_modules/typescript/lib/ru',
-  'node_modules/typescript/lib/tr',
-  'node_modules/typescript/lib/zh-cn',
-  'node_modules/typescript/lib/zh-tw',
-  'node_modules/typescript/lib/README.md',
-  'node_modules/typescript/lib/tsc.js',
-  'node_modules/typescript/lib/tsserverlibrary.js',
-  'node_modules/typescript/lib/tsserverlibrary.d.ts',
-  'node_modules/typescript/lib/typescript.js',
-  'node_modules/typescript/lib/typescript.d.ts',
-  'node_modules/typescript/lib/typescriptServices.js',
-  'node_modules/typescript/lib/typescriptServices.d.ts',
-  'node_modules/typescript/lib/protocol.d.ts',
-  'node_modules/typescript/lib/typingsInstaller.js',
-]
 
 const __dirname = dirname(fileURLToPath(import.meta.url))
 const root = path.join(__dirname, '..')
 const extension = path.join(root, 'packages', 'extension')
+const gitWorker = path.join(root, 'packages', 'git-worker')
 
 fs.rmSync(join(root, 'dist'), { recursive: true, force: true })
 
@@ -52,6 +17,7 @@ const packageJson = JSON.parse(
 )
 delete packageJson.jest
 delete packageJson.devDependencies
+delete packageJson.scripts
 
 fs.writeFileSync(
   join(root, 'dist', 'package.json'),
@@ -67,35 +33,9 @@ fs.cpSync(join(extension, 'src'), join(root, 'dist', 'src'), {
   recursive: true,
 })
 
-const getAllDependencies = (obj) => {
-  if (!obj || !obj.dependencies) {
-    return []
-  }
-  return [obj, ...Object.values(obj.dependencies).flatMap(getAllDependencies)]
-}
-
-const getDependencies = () => {
-  const stdout = execSync('npm list --omit=dev --parseable --all', {
-    cwd: extension,
-  }).toString()
-  const lines = stdout.split('\n')
-  return lines.slice(1, -1)
-}
-
-const dependencies = getDependencies()
-for (const dependency of dependencies) {
-  fs.cpSync(
-    dependency,
-    join(root, 'dist', dependency.slice(extension.length)),
-    {
-      recursive: true,
-    }
-  )
-}
-
-for (const notNeeded of NOT_NEEDED) {
-  fs.rmSync(join(root, 'dist', notNeeded), { force: true, recursive: true })
-}
+fs.cpSync(join(gitWorker, 'src'), join(root, 'dist', 'git-worker', 'src'), {
+  recursive: true,
+})
 
 await packageExtension({
   highestCompression: true,
