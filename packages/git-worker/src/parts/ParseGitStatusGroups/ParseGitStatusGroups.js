26798fb0f3997eec92b3f3b1ba8c499bee7bfcff
Le Vivilet <levivlet@pm.me>
2023-03-19 15:29:27 +0100

    feature: add functions to parse git status groups, separating changes from staged changes

---
+++ packages/git-worker/src/parts/ParseGitStatusGroups/ParseGitStatusGroups.js
@@ -0,0 +1,39 @@
+import * as FileStateType from '../FileStateType/FileStateType.js'
+
+export const parseGitStatusGroups = (files) => {
+  const indexGroup = []
+  const mergeGroup = []
+  const workingTreeGroup = []
+  const untrackedGroup = []
+  for (const file of files) {
+    const { status } = file
+    switch (status) {
+      case FileStateType.Untracked:
+      case FileStateType.Ignored:
+      case FileStateType.IndexModified:
+      case FileStateType.IndexAdded:
+      case FileStateType.IndexDeleted:
+      case FileStateType.IndexRenamed:
+      case FileStateType.IndexCopied:
+        indexGroup.push(file)
+        break
+      case FileStateType.BothDeleted:
+      case FileStateType.AddedByUs:
+      case FileStateType.DeletedByThem:
+      case FileStateType.AddedByThem:
+      case FileStateType.DeletedByUs:
+      case FileStateType.BothAdded:
+      case FileStateType.BothModified:
+        mergeGroup.push(file)
+        break
+      default:
+        throw new Error(`unexpected file status: ${status}`)
+    }
+  }
+  return {
+    indexGroup,
+    mergeGroup,
+    workingTreeGroup,
+    untrackedGroup,
+  }
+}
