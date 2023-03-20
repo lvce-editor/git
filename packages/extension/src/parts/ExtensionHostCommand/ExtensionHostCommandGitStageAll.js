fe509c8fabb76ffbfbc8fd38b3323f2017ce7ede
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2023-03-20 16:29:38 +0100

    fix: git stage command id (#56)

--- packages/extension/src/parts/ExtensionHostCommand/ExtensionHostCommandGitStageAll.js
+++ packages/extension/src/parts/ExtensionHostCommand/ExtensionHostCommandGitStageAll.js
@@ -3,7 +3,7 @@ import * as Repositories from '../GitRepositories/GitRepositories.js'
 import * as GitRepositoriesRequests from '../GitRepositoriesRequests/GitRepositoriesRequests.js'
 import * as GitRequests from '../GitRequests/GitRequests.js'
 
-export const id = CommandId.GitStage
+export const id = CommandId.GitStageAll
 
 /**
  * @param {string} file
