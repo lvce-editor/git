241303e0421a0bb78ef90ef949c7bd89a88858c1
Le Vivilet <levivlet@pm.me>
2023-06-02 23:43:41 +0200

    fix failing test

--- packages/e2e/src/git.pull-error-cannot-lock-ref.js
+++ packages/e2e/src/git.pull-error-cannot-lock-ref.js
@@ -10,9 +10,12 @@ const exec = (command, args, options) => {
       }
     }
     if (args[0] === 'pull') {
-      throw new Error(
-        `error: cannot lock ref 'refs/remotes/origin/master': is at 2e4bfdb24fd137a1d2e87bd480f283cf7001f19a but expected 70ea06a46fd4b38bdba9ab1d64f3fee0f63806a5`
-      )
+      return {
+        stdout: '',
+        stderr: `error: cannot lock ref 'refs/remotes/origin/master': is at 2e4bfdb24fd137a1d2e87bd480f283cf7001f19a but expected 70ea06a46fd4b38bdba9ab1d64f3fee0f63806a5
+! 70ea06a46f..2e4bfdb24f  master     -> origin/master  (unable to update local ref)`,
+        exitCode: 128,
+      }
     }
   }
   throw new Error(`unexpected command ${command}`)
