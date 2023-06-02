9cd732a09c8a65f70d7eb718021c80fe6a2aa272
Le Vivilet <levivlet@pm.me>
2023-06-02 23:47:05 +0200

    fix failing test

--- packages/e2e/src/git.pull-error-divergent-branches.js
+++ packages/e2e/src/git.pull-error-divergent-branches.js
@@ -10,9 +10,26 @@ const exec = (command, args, options) => {
       }
     }
     if (args[0] === 'pull') {
-      throw new Error(
-        `hint: You have divergent branches and need to specify how to reconcile them.`
-      )
+      return {
+        stdout: `> git pull --tags origin main
+From https://github.com/user/repo
+  * branch              main       -> FETCH_HEAD`,
+        stderr: `hint: You have divergent branches and need to specify how to reconcile them.
+hint: You can do so by running one of the following commands sometime before
+hint: your next pull:
+hint:
+hint:   git config pull.rebase false  # merge
+hint:   git config pull.rebase true   # rebase
+hint:   git config pull.ff only       # fast-forward only
+hint:
+hint: You can replace "git config" with "git config --global" to set a default
+hint: preference for all repositories. You can also pass --rebase, --no-rebase,
+hint: or --ff-only on the command line to override the configured default per
+hint: invocation.
+fatal: Need to specify how to reconcile divergent branches.
+`,
+        exitCode: 128,
+      }
     }
   }
   throw new Error(`unexpected command ${command}`)
