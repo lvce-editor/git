7ac791cefd10c6302a852b4f31d80f1161021707
Le Vivilet <levivlet@pm.me>
2023-12-08 22:01:49 +0100

    fix icon path

--- scripts/build.js
+++ scripts/build.js
@@ -56,9 +56,9 @@ replace({
 })
 
 replace({
-  path: join(root, 'dist', 'git-worker', 'src', 'parts', 'IconType', 'IconType.js'),
-  occurrence: '/extension/icons',
-  replacement: '/icons',
+  path: join(root, 'dist', 'git-worker', 'src', 'parts', 'IconRoot', 'IconRoot.js'),
+  occurrence: '/extension',
+  replacement: '',
 })
 
 await rm(join(root, 'dist', 'node', 'node_modules', '.bin'), { recursive: true, force: true })
