ddff18985fd1091c085537e069f36a573c2ad745
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2023-12-08 22:07:12 +0100

    fix: icon path in development (#95)

--- packages/git-worker/src/parts/IconRoot/IconRoot.js
+++ packages/git-worker/src/parts/IconRoot/IconRoot.js
@@ -2,7 +2,7 @@ const getIconRoot = () => {
   const uri = import.meta.url
   const parts = uri.split('/')
   const relevantParts = parts.slice(0, -5)
-  return relevantParts.join('/')
+  return relevantParts.join('/') + '/extension'
 }
 
 export const iconRoot = getIconRoot() + '/icons/dark'
