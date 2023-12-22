6d3bb8d191286d2706062dd58c2bf93b62741374
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2023-12-22 09:53:27 +0100

    refactor: move git worker launch into separate file (#99)

--- packages/extension/src/parts/GitWorker/GitWorker.js
+++ packages/extension/src/parts/GitWorker/GitWorker.js
@@ -1,5 +1,4 @@
-import * as GitWorkerUrl from '../GitWorkerUrl/GitWorkerUrl.js'
-import * as Command from '../Command/Command.js'
+import * as LaunchGitWorker from '../LaunchGitWorker/LaunchGitWorker.js'
 
 export const state = {
   ipc: undefined,
@@ -9,21 +8,9 @@ export const state = {
   rpcPromise: undefined,
 }
 
-const createRpc = async () => {
-  const workerUrl = GitWorkerUrl.getGitWorkerUrl()
-  console.log({ workerUrl })
-  const rpc = await vscode.createRpc({
-    type: 'worker',
-    url: workerUrl,
-    name: 'Git Worker',
-    execute: Command.execute,
-  })
-  return rpc
-}
-
 const getOrCreateRpc = async () => {
   if (!state.rpcPromise) {
-    state.rpcPromise = createRpc()
+    state.rpcPromise = LaunchGitWorker.launchGitWorker()
   }
   return state.rpcPromise
 }
