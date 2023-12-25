2ddcc2b52ba1984c726cd59debfe4ea3bede13a1
Le Vivilet <levivlet@pm.me>
2023-12-25 14:50:49 +0100

    fix getgroups command

--- packages/extension/src/parts/GetGroups/GetGroups.js
+++ packages/extension/src/parts/GetGroups/GetGroups.js
@@ -1,9 +1,11 @@
 import * as Assert from '../Assert/Assert.js'
+import * as Repositories from '../GitRepositories/GitRepositories.js'
 import * as GitWorker from '../GitWorker/GitWorker.js'
 import * as GitWorkerCommandType from '../GitWorkerCommandType/GitWorkerCommandType.js'
 
 export const getGroups = async (cwd) => {
-  const result = await GitWorker.invoke(GitWorkerCommandType.GitGetGroups, cwd)
+  const repository = await Repositories.getCurrent()
+  const result = await GitWorker.invoke(GitWorkerCommandType.GitGetGroups, { gitPath: repository.gitPath, cwd: repository.path })
   Assert.array(result)
   return result
 }
