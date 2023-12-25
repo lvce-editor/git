a7dfe6eb90f0a9f3d5670501feda87e70f7879a4
Le Vivilet <levivlet@pm.me>
2023-12-25 12:23:20 +0100

    fix test

--- packages/git-requests/test/GitRequestsPush.test.js
+++ packages/git-requests/test/GitRequestsPush.test.js
@@ -1,21 +1,4 @@
-import { jest } from '@jest/globals'
-
-beforeEach(() => {
-  jest.resetAllMocks()
-})
-
-jest.unstable_mockModule('../src/parts/Exec/Exec.js', () => {
-  return {
-    exec: jest.fn(() => {
-      throw new Error('not implemented')
-    }),
-  }
-})
-
-const GitRequestsPush = await import(
-  '../src/parts/GitRequestsPush/GitRequestsPush.js'
-)
-const Exec = await import('../src/parts/Exec/Exec.js')
+import * as GitRequestsPush from '../src/parts/GitRequestsPush/GitRequestsPush.js'
 
 class ExecError extends Error {
   constructor(stderr) {
@@ -25,50 +8,47 @@ class ExecError extends Error {
 }
 
 test('push - error - push rejected', async () => {
-  // @ts-ignore
-  Exec.exec.mockImplementation(() => {
+  const exec = () => {
     throw new ExecError('error: failed to push some refs to')
-  })
+  }
   await expect(
     GitRequestsPush.push({
       cwd: '/test/test-folder',
       gitPath: '',
-    })
+      exec,
+    }),
   ).rejects.toThrow(new Error('Git: error: failed to push some refs to'))
 })
 
 test('push - error - could not read from remote repository', async () => {
-  // @ts-ignore
-  Exec.exec.mockImplementation(() => {
+  const exec = () => {
     throw new ExecError('Could not read from remote repository')
-  })
+  }
 
   await expect(
     GitRequestsPush.push({
       cwd: '/test/test-folder',
       gitPath: '',
-    })
+      exec,
+    }),
   ).rejects.toThrow(new Error('Git: Could not read from remote repository'))
 })
 
 test('push - error - no upstream branch', async () => {
-  // @ts-ignore
-  Exec.exec.mockImplementation(() => {
+  const exec = () => {
     throw new ExecError('fatal: The current branch abc has no upstream branch')
-  })
+  }
   await expect(
     GitRequestsPush.push({
       cwd: '/test/test-folder',
       gitPath: '',
-    })
-  ).rejects.toThrowError(
-    new Error('Git: fatal: The current branch abc has no upstream branch')
-  )
+      exec,
+    }),
+  ).rejects.toThrow(new Error('Git: fatal: The current branch abc has no upstream branch'))
 })
 
 test('push - error - no configured push destination', async () => {
-  // @ts-ignore
-  Exec.exec.mockImplementation(() => {
+  const exec = () => {
     throw new ExecError(`fatal: No configured push destination.
     Either specify the URL from the command-line or configure a remote repository using
 
@@ -78,39 +58,38 @@ test('push - error - no configured push destination', async () => {
 
         git push <name>
     `)
-  })
+  }
   await expect(
     GitRequestsPush.push({
       cwd: '/test/test-folder',
       gitPath: '',
-    })
-  ).rejects.toThrowError(
-    new Error(`Git: fatal: No configured push destination.`)
-  )
+      exec,
+    }),
+  ).rejects.toThrow(new Error(`Git: fatal: No configured push destination.`))
 })
 
 test('push - error - permission denied', async () => {
-  // @ts-ignore
-  Exec.exec.mockImplementation(() => {
+  const exec = () => {
     throw new ExecError('Permission denied')
-  })
+  }
   await expect(
     GitRequestsPush.push({
       cwd: '/test/test-folder',
       gitPath: '',
-    })
+      exec,
+    }),
   ).rejects.toThrow(new Error('Git: Permission denied'))
 })
 
 test('push - error - unknown git error', async () => {
-  // @ts-ignore
-  Exec.exec.mockImplementation(() => {
+  const exec = () => {
     throw new ExecError('oops')
-  })
+  }
   await expect(
     GitRequestsPush.push({
       cwd: '/test/test-folder',
       gitPath: '',
-    })
-  ).rejects.toThrowError(new Error('Git: oops'))
+      exec,
+    }),
+  ).rejects.toThrow(new Error('Git: oops'))
 })
