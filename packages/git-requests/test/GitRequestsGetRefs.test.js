4564fb77d95b025a7c5f778f340f55c23e0c39a1
Le Vivilet <levivlet@pm.me>
2023-12-25 12:21:50 +0100

    fix test

--- packages/git-requests/test/GitRequestsGetRefs.test.js
+++ packages/git-requests/test/GitRequestsGetRefs.test.js
@@ -1,25 +1,10 @@
-import { jest } from '@jest/globals'
-
-beforeEach(() => {
-  jest.resetAllMocks()
-})
-
-jest.unstable_mockModule('../src/parts/Exec/Exec.js', () => {
-  return {
-    exec: jest.fn(() => {
-      throw new Error('not implemented')
-    }),
-  }
-})
-
-const GitRequestsGetRefs = await import('../src/parts/GitRequestsGetRefs/GitRequestsGetRefs.js')
-const Exec = await import('../src/parts/Exec/Exec.js')
+import * as GitRequestsGetRefs from '../src/parts/GitRequestsGetRefs/GitRequestsGetRefs.js'
 
 // TODO mock exec instead
 
 test.skip('getRefs', async () => {
   // @ts-ignore
-  Exec.exec.mockImplementation(() => {
+  const exec = () => {
     return {
       stdout: `refs/remotes/origin/HEAD 903f9903f4f14e0d7ec1a389b9da617848e7f609\u0020
 refs/remotes/origin/main 903f9903f4f14e0d7ec1a389b9da617848e7f609\u0020
@@ -27,11 +12,12 @@ refs/remotes/origin/sandy081/powerful-flea 903f9903f4f14e0d7ec1a389b9da617848e7f
 refs/remotes/origin/lszomoru/product-build-parallel 7ed03031bb8511eada0f8418550e33a70e208106\u0020`,
       stderr: '',
     }
-  })
+  }
   expect(
     await GitRequestsGetRefs.getRefs({
       cwd: '/test/test-folder',
       gitPath: '',
+      exec,
     }),
   ).toEqual([
     {
@@ -62,14 +48,14 @@ refs/remotes/origin/lszomoru/product-build-parallel 7ed03031bb8511eada0f8418550e
 })
 
 test('getRefs - error', async () => {
-  // @ts-ignore
-  Exec.exec.mockImplementation(() => {
+  const exec = () => {
     throw new TypeError(`x is not a function`)
-  })
+  }
   await expect(
     GitRequestsGetRefs.getRefs({
       cwd: '/test/test-folder',
       gitPath: '',
+      exec,
     }),
-  ).rejects.toThrowError(new Error('Git: x is not a function'))
+  ).rejects.toThrow(new Error('Git: x is not a function'))
 })
--- packages/git-requests/test/GitRequestsPull.test.js
+++ packages/git-requests/test/GitRequestsPull.test.js
@@ -1,21 +1,7 @@
-import { jest } from '@jest/globals'
 
-beforeEach(() => {
-  jest.resetAllMocks()
-})
 
-jest.unstable_mockModule('../src/parts/Exec/Exec.js', () => {
-  return {
-    exec: jest.fn(() => {
-      throw new Error('not implemented')
-    }),
-  }
-})
-
-const GitRequestsPull = await import(
-  '../src/parts/GitRequestsPull/GitRequestsPull.js'
+import * as GitRequestsPull from '../src/parts/GitRequestsPull/GitRequestsPull.js'
 )
-const Exec = await import('../src/parts/Exec/Exec.js')
 
 class ExecError extends Error {
   constructor(stderr) {
@@ -25,21 +11,20 @@ class ExecError extends Error {
 }
 
 test('pull - error - no user name configured', async () => {
-  // @ts-ignore
-  Exec.exec.mockImplementation(() => {
+  const exec =(() => {
     throw new ExecError('Please tell me who you are.')
   })
   await expect(
     GitRequestsPull.pull({
       cwd: '/test/test-folder',
       gitPath: '',
+      exec
     })
-  ).rejects.toThrowError(new Error('Git: Please tell me who you are.'))
+  ).rejects.toThrow(new Error('Git: Please tell me who you are.'))
 })
 
 test('pull - error - dirty working tree', async () => {
-  // @ts-ignore
-  Exec.exec.mockImplementation(() => {
+  const exec =(() => {
     throw new ExecError(
       'Pulling is not possible because you have unmerged files'
     )
@@ -48,86 +33,87 @@ test('pull - error - dirty working tree', async () => {
     GitRequestsPull.pull({
       cwd: '/test/test-folder',
       gitPath: '',
+      exec
     })
-  ).rejects.toThrowError(
+  ).rejects.toThrow(
     new Error('Git: Pulling is not possible because you have unmerged files')
   )
 })
 
 test('pull - error - cannot lock ref (1)', async () => {
-  // @ts-ignore
-  Exec.exec.mockImplementation(() => {
+  const exec =(() => {
     throw new ExecError('cannot lock ref')
   })
   await expect(
     GitRequestsPull.pull({
       cwd: '/test/test-folder',
       gitPath: '',
+      exec
     })
   ).rejects.toThrow(new Error('Git: cannot lock ref'))
 })
 
 test('pull - error - cannot lock ref (2)', async () => {
-  // @ts-ignore
-  Exec.exec.mockImplementation(() => {
+  const exec =(() => {
     throw new ExecError('unable to update local ref')
   })
   await expect(
     GitRequestsPull.pull({
       cwd: '/test/test-folder',
       gitPath: '',
+      exec
     })
   ).rejects.toThrow(new Error('Git: unable to update local ref'))
 })
 
 test('pull - error - cannot rebase onto multiple branches', async () => {
-  // @ts-ignore
-  Exec.exec.mockImplementation(() => {
+  const exec =(() => {
     throw new ExecError('cannot rebase onto multiple branches')
   })
   await expect(
     GitRequestsPull.pull({
       cwd: '/test/test-folder',
       gitPath: '',
+      exec
     })
   ).rejects.toThrow(new Error('Git: cannot rebase onto multiple branches'))
 })
 
 test('pull - error - remote connection error', async () => {
-  // @ts-ignore
-  Exec.exec.mockImplementation(() => {
+  const exec =(() => {
     throw new ExecError('Could not read from remote repository')
   })
   await expect(
     GitRequestsPull.pull({
       cwd: '/test/test-folder',
       gitPath: '',
+      exec
     })
   ).rejects.toThrow(new Error('Git: Could not read from remote repository'))
 })
 
 test('pull - error - not a git repository', async () => {
-  // @ts-ignore
-  Exec.exec.mockImplementation(() => {
+  const exec =(() => {
     throw new ExecError('fatal: not a git repository')
   })
   await expect(
     GitRequestsPull.pull({
       cwd: '/test/test-folder',
       gitPath: '',
+      exec
     })
   ).rejects.toThrow(new Error('Git: fatal: not a git repository'))
 })
 
 test('pull - error - unknown git error', async () => {
-  // @ts-ignore
-  Exec.exec.mockImplementation(() => {
+  const exec =(() => {
     throw new ExecError('oops')
   })
   await expect(
     GitRequestsPull.pull({
       cwd: '/test/test-folder',
       gitPath: '',
+      exec
     })
   ).rejects.toThrow(new Error('Git: oops'))
 })
