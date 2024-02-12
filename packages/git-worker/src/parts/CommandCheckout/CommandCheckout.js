7fca7d5d954da024bfb8b551b442dd6fd0bc7d78
Le Vivilet <levivlet@pm.me>
2024-02-12 18:29:50 +0100

    feature: add checkout command to git worker

---
+++ packages/git-worker/src/parts/CommandCheckout/CommandCheckout.js
@@ -0,0 +1,52 @@
+import * as Exec from '../Exec/Exec.js'
+import * as Repositories from '../GitRepositories/GitRepositories.js'
+import * as GitRepositoriesRequests from '../GitRepositoriesRequests/GitRepositoriesRequests.js'
+import * as GitRequests from '../GitRequests/GitRequests.js'
+import * as Rpc from '../Rpc/Rpc.js'
+
+const getShortCommit = (commit) => {
+  return commit.slice(0, 8)
+}
+
+const toPick = (ref) => {
+  return {
+    label: ref.name,
+    description: getShortCommit(ref.commit),
+  }
+}
+
+const getPicks = async () => {
+  const repository = await Repositories.getCurrent()
+  const refs = await GitRepositoriesRequests.execute({
+    id: 'getRefs',
+    fn: GitRequests.getRefs,
+    args: {
+      cwd: repository.path,
+      gitPath: repository.gitPath,
+      exec: Exec.exec,
+    },
+  })
+  return refs
+}
+
+export const commandCheckout = async () => {
+  const rawPicks = await getPicks()
+  const picks = rawPicks.map(toPick)
+  const selectedPick = await Rpc.invoke('QuickPick.show', picks)
+  if (!selectedPick) {
+    return
+  }
+  const { label } = selectedPick
+  const repository = await Repositories.getCurrent()
+  await GitRepositoriesRequests.execute({
+    id: 'checkout',
+    fn: GitRequests.checkout,
+    args: {
+      cwd: repository.path,
+      gitPath: repository.gitPath,
+      ref: label,
+      exec: Exec.exec,
+    },
+  })
+  // console.log({ selectedPick })
+}
