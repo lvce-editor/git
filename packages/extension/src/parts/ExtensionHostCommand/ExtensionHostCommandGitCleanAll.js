114bc4612b1385371a9185daffac4d2644f26324
Le Vivilet <levivlet@pm.me>
2024-02-12 19:18:53 +0100

    remove sync

--- packages/extension/src/parts/ExtensionHostCommand/ExtensionHostCommandGitCleanAll.js
+++ packages/extension/src/parts/ExtensionHostCommand/ExtensionHostCommandGitCleanAll.js
@@ -3,6 +3,6 @@ import * as GitWorker from '../GitWorker/GitWorker.js'
 
 export const id = CommandId.GitCleanAll
 
-export const execute = async () => {
+export const execute = () => {
   return GitWorker.invoke('Command.gitCleanAll')
 }
