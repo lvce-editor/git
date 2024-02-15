1b78e00daabea6b827a8a219955b852a199e63d8
Le Vivilet <levivlet@pm.me>
2024-02-15 22:31:59 +0100

    fix test

--- packages/extension/test/GitRepositories.test.js
+++
@@ -1,9 +0,0 @@
-// some tests are based on https://github.com/eclipse-theia/theia/blob/master/packages/git/src/node/dugite-git.spec.ts by eclipse-theia (LICENSE EPL-2.0 OR GPL-2.0 WITH Classpath-exception-2.0)
-
-import { mkdir } from 'fs/promises'
-import { join } from 'path'
-
-test.skip('should only discover first repository', async () => {
-  // const root = await getTmpDir()
-  // await mkdir(join(root, 'A'))
-})
