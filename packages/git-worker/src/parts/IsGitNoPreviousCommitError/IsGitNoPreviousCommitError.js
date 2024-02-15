1d03e8a282726fb6208e07fae694c244737b86d7
Le Vivilet <levivlet@pm.me>
2024-02-15 19:38:36 +0100

    refactor: remove unused files from git worker

--- packages/git-worker/src/parts/IsGitNoPreviousCommitError/IsGitNoPreviousCommitError.js
+++
@@ -1,9 +0,0 @@
-export const isGitNoRepositoryError = (error) => {
-  return (
-    error &&
-    error instanceof Error &&
-    error.message.includes(
-      `fatal: ambiguous argument 'HEAD~1': unknown revision or path not in the working tree.`
-    )
-  )
-}
