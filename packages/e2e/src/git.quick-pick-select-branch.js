01c10f7858088247e5ee9876d8a3be873bfab74c
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-02-15 23:31:08 +0100

    feature: improve git branch pick test (#181)

--- packages/e2e/src/git.quick-pick-select-branch.js
+++ packages/e2e/src/git.quick-pick-select-branch.js
@@ -2,20 +2,92 @@ export const skip = true
 
 export const name = 'quick-pick-select-branch'
 
-export const test = async ({ FileSystem, Workspace, Settings, QuickPick, Locator, expect }) => {
+const gitVersion = () => {
+  return {
+    stdout: '0.0.0',
+    stderr: '',
+    exitCode: 0,
+  }
+}
+
+const gitRevParse = () => {
+  return {
+    stdout: '',
+    stderr: ``,
+    exitCode: 0,
+  }
+}
+
+const gitStatus = () => {
+  return {
+    stdout: ` M test/file-1.txt
+ M test/file-2.txt
+`,
+    stderr: ``,
+    exitCode: 0,
+  }
+}
+
+const gitForEachRef = () => {
+  return {
+    stdout: [
+      `refs/heads/b f63b3b1b60166f6f3a99e5de7867cc2ae29b9c92 `,
+      `refs/heads/main a205f65eaeedfcc0bae56de354220df552d44a52 `,
+      `refs/remotes/origin/b f63b3b1b60166f6f3a99e5de7867cc2ae29b9c92 `,
+      `refs/remotes/origin/main a205f65eaeedfcc0bae56de354220df552d44a52 `,
+    ].join('\n'),
+    stderr: '',
+    exitCode: 0,
+  }
+}
+
+const exec = (command, args, options) => {
+  if (command !== 'git') {
+    throw new Error(`unexpected command ${command}`)
+  }
+  const subCommand = args[0]
+  switch (subCommand) {
+    case '--version':
+      return gitVersion()
+    case 'rev-parse':
+      return gitRevParse()
+    case 'status':
+      return gitStatus()
+    case 'for-each-ref':
+      return gitForEachRef()
+    default:
+      throw new Error(`unexpected command ${subCommand}`)
+  }
+}
+
+export const mockRpc = {
+  name: 'Git',
+  commands: {
+    'Exec.exec': exec,
+  },
+}
+
+export const test = async ({ FileSystem, Workspace, Settings, QuickPick, SideBar }) => {
   // arrange
   const tmpDir = await FileSystem.getTmpDir({ scheme: 'file' })
   await Workspace.setPath(tmpDir)
-  const gitPath = await FileSystem.createExecutableFrom(``)
-  await Settings.update({
-    'git.path': gitPath,
-  })
+  await SideBar.open('Source Control')
 
   // act
   await QuickPick.open()
-  await QuickPick.setValue('>Git: Checkout')
-  await QuickPick.selectItem('Git: Checkout')
+  await QuickPick.setValue('>Checkout')
+  await QuickPick.selectItem('Git Checkout')
 
   // assert
-  // TODO verify that branch items are showing, with icon and description
+  await QuickPick.showHaveItems([
+    {
+      label: 'b',
+    },
+    {
+      label: 'main',
+    },
+    {
+      label: 'origin/b',
+    },
+  ])
 }
