c780d0c8a5d59abf1a35bcbba564807c6fb0a061
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-02-15 22:50:13 +0100

    refactor: move checkout picks into separate file (#178)

--- packages/git-worker/src/parts/CommandCheckout/CommandCheckout.js
+++ packages/git-worker/src/parts/CommandCheckout/CommandCheckout.js
@@ -1,37 +1,12 @@
+import * as GetCheckoutPicks from '../GetCheckoutPicks/GetCheckoutPicks.js'
 import * as Git from '../Git/Git.js'
 import * as Repositories from '../GitRepositories/GitRepositories.js'
 import * as GitRepositoriesRequests from '../GitRepositoriesRequests/GitRepositoriesRequests.js'
 import * as GitRequests from '../GitRequests/GitRequests.js'
 import * as Rpc from '../Rpc/Rpc.js'
 
-const getShortCommit = (commit) => {
-  return commit.slice(0, 8)
-}
-
-const toPick = (ref) => {
-  return {
-    label: ref.name,
-    description: getShortCommit(ref.commit),
-  }
-}
-
-const getPicks = async () => {
-  const repository = await Repositories.getCurrent()
-  const refs = await GitRepositoriesRequests.execute({
-    id: 'getRefs',
-    fn: GitRequests.getRefs,
-    args: {
-      cwd: repository.path,
-      gitPath: repository.gitPath,
-      exec: Git.exec,
-    },
-  })
-  return refs
-}
-
 export const commandCheckout = async () => {
-  const rawPicks = await getPicks()
-  const picks = rawPicks.map(toPick)
+  const picks = await GetCheckoutPicks.getCheckoutPicks()
   const selectedPick = await Rpc.invoke('QuickPick.show', picks)
   if (!selectedPick) {
     return
