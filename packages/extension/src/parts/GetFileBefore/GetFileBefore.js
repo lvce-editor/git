5de288399a2063ac26722b821d2c12dc095f689f
Le Vivilet <levivlet@pm.me>
2024-02-15 21:58:57 +0100

    refactor: move git groups to git worker

--- packages/extension/src/parts/GetFileBefore/GetFileBefore.js
+++ packages/extension/src/parts/GetFileBefore/GetFileBefore.js
@@ -1,11 +1,9 @@
 import * as Assert from '../Assert/Assert.js'
-import * as Repositories from '../GitRepositories/GitRepositories.js'
 import * as GitWorker from '../GitWorker/GitWorker.js'
 import * as GitWorkerCommandType from '../GitWorkerCommandType/GitWorkerCommandType.js'
 
 export const getFileBefore = async (uri) => {
   Assert.string(uri)
-  const repository = await Repositories.getCurrent()
-  const result = await GitWorker.invoke(GitWorkerCommandType.GitGetFileBefore, { gitPath: repository.gitPath, repositoryPath: repository.path, uri })
+  const result = await GitWorker.invoke(GitWorkerCommandType.GitGetFileBefore, { uri })
   return result
 }
