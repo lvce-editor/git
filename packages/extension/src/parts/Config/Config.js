64635aaaa6de981761f8ea2268975377cd01c0e8
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-02-15 21:55:08 +0100

    feature: move repositories to git worker (#173)

--- packages/extension/src/parts/Config/Config.js
+++ packages/extension/src/parts/Config/Config.js
@@ -2,3 +2,16 @@ export const getWorkspaceFolder = () => {
   const path = vscode.getWorkspaceFolder()
   return path
 }
+
+export const getGitPaths = () => {
+  // TODO don't couple vscode api with business logic too much
+  const configuredGitPath = vscode.getConfiguration('git.path')
+  if (configuredGitPath) {
+    return [configuredGitPath]
+  }
+  const paths = ['git']
+  if (vscode.env.GIT_PATH) {
+    paths.unshift(vscode.env.GIT_PATH)
+  }
+  return paths
+}
