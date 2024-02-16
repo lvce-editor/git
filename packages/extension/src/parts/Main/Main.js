4d6c137301167eaaa09f781ddd7a41c945b3339e
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-02-16 22:25:59 +0100

    refactor: remove unused code (#185)

--- packages/extension/src/parts/Main/Main.js
+++ packages/extension/src/parts/Main/Main.js
@@ -1,23 +1,6 @@
 import * as ExtensionHostCommand from '../ExtensionHostCommand/ExtensionHostCommand.js'
-import * as GitFind from '../GitFind/GitFind.js'
-import * as OutputChannelGit from '../UiOutputChannel/UiOutputChannelGit.js'
-import * as SourceControlProviderGit from '../UiSourceControlProvider/UiSourceControlProviderGit.js'
 import * as ExtensionInfo from '../ExtensionInfo/ExtensionInfo.js'
-
-export const initializeProject = async () => {
-  const git = await GitFind.findGit()
-  if (!git) {
-    console.info('[git] git not found')
-    OutputChannelGit.append('git not found')
-    // TODO should modeless status bar item
-    // or item in output channel
-    // that git could not be found
-    return
-  }
-  OutputChannelGit.append(`using git version ${git.version}\n`)
-  // const folder = vscode.getWorkspaceFolder()
-  // const repository = await Repositories.getCurrent()
-}
+import * as SourceControlProviderGit from '../UiSourceControlProvider/UiSourceControlProviderGit.js'
 
 export const activate = async ({ path }) => {
   for (const command of Object.values(ExtensionHostCommand)) {
@@ -26,8 +9,6 @@ export const activate = async ({ path }) => {
 
   ExtensionInfo.setPath(path)
   vscode.registerSourceControlProvider(SourceControlProviderGit)
-
-  await initializeProject()
 
   // vscode.registerStatusBarItem(StatusBarCheckout.create())
   // vscode.registerStatusBarItem(StatusBarItemSync)
