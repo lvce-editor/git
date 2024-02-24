b546b9603661bd2f2e665cdc365a679b566c0f0d
Le Vivilet <levivlet@pm.me>
2024-02-24 17:35:32 +0100

    fix test

--- packages/integration/test/git-version.test.ts
+++ packages/integration/test/git-version.test.ts
@@ -18,3 +18,24 @@ test('git version', async () => {
   })
   expect(await worker.execute('Git.version', { gitPath: '', cwd: '' })).toBe('2.39.2')
 })
+
+test('git version - fatal unterminated line', async () => {
+  const execMap = {
+    '--version': {
+      stdout: '',
+      stderr: 'fatal: unterminated line in .git/packed-refs: 17171717171717171717171717171717171717',
+      exitCode: 1,
+    },
+  }
+  const config = {
+    gitPaths: ['git'],
+    workspaceFolder: '/test',
+  }
+  const worker = await testWorker({
+    execMap,
+    config,
+  })
+  await expect(worker.execute('Git.version', { gitPath: '', cwd: '' })).rejects.toThrow(
+    new Error(`Git: fatal: unterminated line in .git/packed-refs: 17171717171717171717171717171717171717`),
+  )
+})
