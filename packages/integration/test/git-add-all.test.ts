368f48ac57004d3ad97f80e84948999c82beea63
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-02-24 08:49:34 +0100

    fix: opening diff view (#198)

--- packages/integration/test/git-add-all.test.ts
+++ packages/integration/test/git-add-all.test.ts
@@ -2,23 +2,36 @@ import { testWorker } from '../src/testWorker.js'
 
 test('git add all', async () => {
   const execMap = {
+    '--version': {
+      stdout: 'git version 2.39.2',
+      stderr: '',
+      exitCode: 0,
+    },
     add: {
       stdout: '',
       stderr: '',
       exitCode: 0,
     },
   }
+  const config = {
+    gitPaths: ['git'],
+    workspaceFolder: '/test',
+  }
   const worker = await testWorker({
     execMap,
+    config,
   })
   await worker.execute('Git.addAll', { gitPath: 'git', cwd: '' })
   expect(worker.invocations).toEqual([
+    ['Config.getGitPaths'],
+    ['Exec.exec', 'git', ['--version'], {}],
+    ['Config.getWorkspaceFolder'],
     [
       'Exec.exec',
       'git',
       ['add', '.'],
       {
-        cwd: '',
+        cwd: '/test',
         env: {
           GIT_OPTIONAL_LOCKS: '0',
           LANG: 'en_US.UTF-8',
