9d2aca1a703f97c44ea033bda17581aa0f4d1b19
Le Vivilet <levivlet@pm.me>
2024-02-24 17:30:56 +0100

    test

--- packages/git-requests/test/GitRequestsDiscard.test.js
+++ packages/git-requests/test/GitRequestsDiscard.test.js
@@ -3,11 +3,13 @@ import { jest } from '@jest/globals'
 
 test('discard', async () => {
   const exec = jest.fn()
+  const confirm = jest.fn()
   await GitRequestsDiscard.discard({
     cwd: '/test/test-folder',
     gitPath: 'git',
     file: 'index.js',
     exec,
+    confirm,
   })
   expect(exec).toHaveBeenCalledTimes(1)
   expect(exec).toHaveBeenCalledWith({ args: ['restore', '--', 'index.js'], cwd: '/test/test-folder', gitPath: 'git', name: 'discard' })
