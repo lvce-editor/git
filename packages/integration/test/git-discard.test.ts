31f2c1da589ad7ec47aef1fa5e92d3035a4b68b5
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-02-24 11:26:59 +0100

    feature: add git discard integration test (#204)

---
+++ packages/integration/test/git-discard.test.ts
@@ -0,0 +1,49 @@
+import { testWorker } from '../src/testWorker.js'
+
+test('git add all', async () => {
+  const execMap = {
+    '--version': {
+      stdout: 'git version 2.39.2',
+      stderr: '',
+      exitCode: 0,
+    },
+    add: {
+      stdout: '',
+      stderr: '',
+      exitCode: 0,
+    },
+    restore: {
+      stdout: '',
+      stderr: '',
+      exitCode: 0,
+    },
+  }
+  const config = {
+    gitPaths: ['git'],
+    workspaceFolder: '/test',
+  }
+  const worker = await testWorker({
+    execMap,
+    config,
+  })
+  await worker.execute('Git.discard', { gitPath: 'git', cwd: '', file: '/test/file.txt' })
+  expect(worker.invocations).toEqual([
+    ['Config.getGitPaths'],
+    ['Exec.exec', 'git', ['--version'], {}],
+    ['Config.getWorkspaceFolder'],
+    [
+      'Exec.exec',
+      'git',
+      ['restore', '--', '/test/file.txt'],
+      {
+        cwd: '/test',
+        env: {
+          GIT_OPTIONAL_LOCKS: '0',
+          LANG: 'en_US.UTF-8',
+          LC_ALL: 'en_US',
+        },
+        reject: false,
+      },
+    ],
+  ])
+})
