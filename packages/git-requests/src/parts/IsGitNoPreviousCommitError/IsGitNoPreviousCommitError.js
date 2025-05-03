66d38fcb89374b4ef3583577e752b43f0f1dcd35
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-05-03 22:51:07 +0200

    fix: git unstage with empty repository (#240)

--- packages/git-requests/src/parts/IsGitNoPreviousCommitError/IsGitNoPreviousCommitError.js
+++ packages/git-requests/src/parts/IsGitNoPreviousCommitError/IsGitNoPreviousCommitError.js
@@ -1,9 +1,5 @@
 export const isGitNoRepositoryError = (error) => {
   return (
-    error &&
-    error instanceof Error &&
-    error.message.includes(
-      `fatal: ambiguous argument 'HEAD~1': unknown revision or path not in the working tree.`
-    )
+    error && error instanceof Error && error.message.includes(`fatal: ambiguous argument 'HEAD~1': unknown revision or path not in the working tree.`)
   )
 }
