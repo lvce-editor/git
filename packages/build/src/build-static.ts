d994bc082e8fc9f4fce916f3d03cf3a9108a86f8
Le Vivilet <levivilet@pm.me>
2025-09-10 18:52:20 +0200

    feature: move build to typescript (#267)
    * feature: move build to typescript
    
    * Discard changes to packages/build/package.json
    
    * build

--- packages/build/src/build-static.ts
+++ packages/build/src/build-static.ts
@@ -1,9 +1,9 @@
 import { exportStatic } from '@lvce-editor/shared-process'
 import { cp, readdir } from 'node:fs/promises'
 import path from 'node:path'
-import { root } from './root.js'
+import { root } from './root.ts'
 
-await import('./build.js')
+await import('./build.ts')
 
 await cp(path.join(root, 'dist'), path.join(root, 'dist2'), {
   recursive: true,
@@ -17,8 +17,8 @@ await exportStatic({
 })
 
 const RE_COMMIT_HASH = /^[a-z\d]+$/
-const isCommitHash = (dirent) => {
-  return dirent.length === 7 && dirent.match(RE_COMMIT_HASH)
+const isCommitHash = (dirent: string): boolean => {
+  return dirent.length === 7 && dirent.match(RE_COMMIT_HASH) !== null
 }
 
 const dirents = await readdir(path.join(root, 'dist'))
