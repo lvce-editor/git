0283ae2e13026952fc78aa0feefaee3ca1085651
Le Vivilet <levivilet@pm.me>
2025-09-11 10:44:10 +0200

    feature: update dev script

---
+++ packages/build/src/build-watch.ts
@@ -0,0 +1,22 @@
+import { execa } from 'execa'
+import { root } from './root.ts'
+import { join } from 'node:path'
+
+const main = async () => {
+  const binaryName = process.platform === 'win32' ? 'esbuild.exe' : 'esbuild'
+  const esbuildPath = join(root, 'packages', 'build', 'node_modules', 'esbuild', 'bin', binaryName)
+  execa(esbuildPath, ['--format=esm', '--bundle', '--watch', 'packages/extension/src/gitMain.js', '--outfile=packages/extension/dist/gitMain.js'], {
+    cwd: root,
+    stdio: 'inherit',
+  })
+  execa(
+    esbuildPath,
+    ['--format=esm', '--bundle', '--watch', 'packages/git-worker/src/gitWorkerMain.ts', '--outfile=packages/git-worker/dist/gitWorkerMain.js'],
+    {
+      cwd: root,
+      stdio: 'inherit',
+    },
+  )
+}
+
+main()
