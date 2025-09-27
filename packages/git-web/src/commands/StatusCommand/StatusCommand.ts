b33b2eff576bdf216de7c708d7e7933cd7e9f405
Le Vivilet <levivilet@pm.me>
2025-09-27 18:55:23 +0200

    tsc

---
+++ packages/git-web/src/commands/StatusCommand/StatusCommand.ts
@@ -0,0 +1,15 @@
+import * as ExitCode from '../../ExitCode/ExitCode.js'
+import { GitRepository } from '../../GitRepository/GitRepository.js'
+import type { CommandOptions } from '../../CommandOptions/CommandOptions.js'
+import type { CommandResult } from '../../CommandResult/CommandResult.js'
+
+export const handleStatus = async (args: string[], options: CommandOptions): Promise<CommandResult> => {
+  const repository = await GitRepository.getRepository(options.cwd)
+  const status = await repository.getStatus()
+  
+  return {
+    stdout: status,
+    stderr: '',
+    exitCode: ExitCode.Success
+  }
+}
