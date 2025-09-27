fd99e363591f54bc67d7a7c5b18366481f745c43
Le Vivilet <levivilet@pm.me>
2025-09-27 21:56:32 +0200

    feature: improve e2e test

--- packages/git-worker/src/parts/CommandLoadFixture/CommandLoadFixture.ts
+++ packages/git-worker/src/parts/CommandLoadFixture/CommandLoadFixture.ts
@@ -1,6 +1,33 @@
+const gitInit = () => {
+  // TODO
+  console.log('init')
+}
+
+const touch = (fileName: string) => {
+  // TODO
+  console.log('touch', fileName)
+}
+
+const executeAction = async (action) => {
+  switch (action.type) {
+    case 'git-init':
+      return gitInit()
+    case 'touch':
+      return touch(action.fileName)
+  }
+}
+
+const executeActions = async (actions) => {
+  for (const action of actions) {
+    await executeAction(action)
+  }
+}
+
 export const commandLoadFixture = async (fixtureUrl: string): Promise<void> => {
   console.log('load fixture', fixtureUrl)
   const importUrl = `${fixtureUrl}/git.js`
   const module = await import(importUrl)
+  const actions = module.actions
+  await executeActions(actions)
   console.log({ module })
 }
