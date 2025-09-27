0c99fe4e49be55abcd22805d27bab43f7fbc53f7
Le Vivilet <levivilet@pm.me>
2025-09-27 23:20:15 +0200

    feature: use exec depending on uri

--- packages/extension/src/parts/GetInvoke/GetInvoke.js
+++ packages/extension/src/parts/GetInvoke/GetInvoke.js
@@ -1,11 +1,9 @@
 import { invoke } from '../GitWebWorker/GitWebWorker.js'
 import * as Rpc from '../Rpc/Rpc.js'
 
-export const getInvoke = async (scheme) => {
-  switch (scheme) {
-    case 'file://':
-      return Rpc.invoke
-    default:
-      return invoke
+export const getInvoke = (scheme) => {
+  if (!scheme || scheme.startsWith('file://')) {
+    return Rpc.invoke
   }
+  return invoke
 }
