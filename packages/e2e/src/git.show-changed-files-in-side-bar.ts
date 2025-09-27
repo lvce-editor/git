2ebfd31aa4e9ea16fae75252dcfa97957dc5fb44
Le Vivilet <levivilet@pm.me>
2025-09-27 20:51:33 +0200

    skip

--- packages/e2e/src/git.show-changed-files-in-side-bar.ts
+++ packages/e2e/src/git.show-changed-files-in-side-bar.ts
@@ -2,6 +2,8 @@ import type { Test } from '@lvce-editor/test-with-playwright'
 
 export const name = 'git.show-changed-files-in-side-bar'
 
+export const skip = 1
+
 export const test: Test = async ({ Command, Extension, SourceControl, FileSystem, Workspace, Settings, SideBar, Locator, expect }) => {
   // arrange
   const tmpDir = await FileSystem.getTmpDir({ scheme: 'file' })
