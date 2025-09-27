942794fa0e243ce0bca1dcc2d32058b936d68b2d
Le Vivilet <levivilet@pm.me>
2025-09-27 19:14:41 +0200

    mockrpc

--- packages/git-web/src/RegisterMockRpc/RegisterMockRpc.ts
+++ packages/git-web/src/RegisterMockRpc/RegisterMockRpc.ts
@@ -1,1 +1,6 @@
-export const registerMockRpc = (commandMap: any): any => {}
+import * as RpcRegistry from '@lvce-editor/rpc-registry'
+import { RpcId } from '../RpcId/RpcId.ts'
+
+export const registerMockRpc = (commandMap: any): RpcRegistry.MockRpc => {
+  return RpcRegistry.registerMockRpc(RpcId, commandMap)
+}
