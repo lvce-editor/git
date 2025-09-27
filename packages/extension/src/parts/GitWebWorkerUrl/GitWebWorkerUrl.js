cfada3388a1b460e7835910e3fc6857252158b08
Le Vivilet <levivilet@pm.me>
2025-09-27 23:07:20 +0200

    feature: improve e2e test (#317)
    * feature: improve e2e test
    
    * test

--- packages/extension/src/parts/GitWebWorkerUrl/GitWebWorkerUrl.js
+++ packages/extension/src/parts/GitWebWorkerUrl/GitWebWorkerUrl.js
@@ -1,3 +1,3 @@
 export const getGitWebWorkerUrl = () => {
-  return new URL('../../../../git-web/src/gitWebMain.ts', import.meta.url).toString()
+  return new URL('../../../../git-web/dist/gitWebMain.js', import.meta.url).toString()
 }
