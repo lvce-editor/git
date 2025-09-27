e14d8bafbbd172e50db5e0d3ed29aaea70111833
Le Vivilet <levivilet@pm.me>
2025-09-27 23:22:14 +0200

    feature: use global rpc as fallback (#321)

--- packages/git-web/src/Rpc/Rpc.ts
+++ packages/git-web/src/Rpc/Rpc.ts
@@ -2,6 +2,6 @@ import * as RpcRegistry from '@lvce-editor/rpc-registry'
 import { RpcId } from '../RpcId/RpcId.ts'
 
 export const invoke = async (method: string, ...parameters: readonly any[]): Promise<any> => {
-  const rpc = RpcRegistry.get(RpcId)
+  const rpc = RpcRegistry.get(RpcId) || globalThis.rpc
   return rpc.invoke(method, ...parameters)
 }
