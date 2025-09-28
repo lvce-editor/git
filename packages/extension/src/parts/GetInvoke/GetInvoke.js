5a2f970d8f1276dba6171898b86b51c7590c1df0
Le Vivilet <levivilet@pm.me>
2025-09-28 18:39:42 +0200

    fix: scheme check (#335)

--- packages/extension/src/parts/GetInvoke/GetInvoke.js
+++ packages/extension/src/parts/GetInvoke/GetInvoke.js
@@ -1,7 +1,18 @@
 import { invoke } from '../GitWebWorker/GitWebWorker.js'
 import * as Rpc from '../Rpc/Rpc.js'
 
-export const getInvoke = (scheme) => {
+const RE_SCHEME = /^[a-z\-]+:\/\//
+
+const getScheme = (uri) => {
+  const scheme = uri.match(RE_SCHEME)
+  if (!scheme) {
+    return ''
+  }
+  return scheme[0]
+}
+
+export const getInvoke = (uri) => {
+  const scheme = getScheme(uri)
   if (!scheme || scheme.startsWith('file://')) {
     return Rpc.invoke
   }
