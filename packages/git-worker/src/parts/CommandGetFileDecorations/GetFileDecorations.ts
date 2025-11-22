1c5b551aac6e52ca145d6a301020e67b9eca53d4
Le Vivilet <levivilet@pm.me>
2025-11-22 18:19:52 +0100

    feature: parse decorations (#363)

--- packages/git-worker/src/parts/CommandGetFileDecorations/GetFileDecorations.ts
+++ packages/git-worker/src/parts/CommandGetFileDecorations/GetFileDecorations.ts
@@ -3,9 +3,28 @@ import * as Repositories from '../GitRepositories/GitRepositories.ts'
 import * as GitRepositoriesRequests from '../GitRepositoriesRequests/GitRepositoriesRequests.ts'
 import * as GitRequests from '../GitRequests/GitRequests.ts'
 
+const parseDecorations = (stdout: string): readonly any[] => {
+  if (!stdout) {
+    return []
+  }
+  const decorations: any[] = []
+  const items = stdout.split('\0')
+  for (let i = 0; i < items.length; i += 4) {
+    const pattern = items[i + 2]
+    const path = items[i + 3]
+    if (pattern && !pattern.startsWith('!')) {
+      decorations.push({
+        decoration: 'ignore',
+        uri: path,
+      })
+    }
+  }
+  return decorations
+}
+
 export const getFileDecorations = async (uris: readonly string[]): Promise<readonly any[]> => {
   const repository = await Repositories.getCurrent()
-  await GitRepositoriesRequests.execute({
+  const gitResult = await GitRepositoriesRequests.execute({
     id: 'decorations',
     fn: GitRequests.getDecorations,
     args: {
@@ -15,6 +34,6 @@ export const getFileDecorations = async (uris: readonly string[]): Promise<reado
       uris,
     },
   })
-  // TODO
-  return []
+  const parsed = parseDecorations(gitResult.stdout)
+  return parsed
 }
