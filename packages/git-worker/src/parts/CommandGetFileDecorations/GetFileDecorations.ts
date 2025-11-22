e98f8d8580fb19b2f9580f5a82c9c6dd2f11cadf
Le Vivilet <levivilet@pm.me>
2025-11-22 17:56:57 +0100

    feature: improve exec options (#360)

--- packages/git-worker/src/parts/CommandGetFileDecorations/GetFileDecorations.ts
+++ packages/git-worker/src/parts/CommandGetFileDecorations/GetFileDecorations.ts
@@ -1,7 +1,20 @@
+import * as Git from '../Git/Git.ts'
 import * as Repositories from '../GitRepositories/GitRepositories.ts'
+import * as GitRepositoriesRequests from '../GitRepositoriesRequests/GitRepositoriesRequests.ts'
+import * as GitRequests from '../GitRequests/GitRequests.ts'
 
-export const getFileDecorations = async (files: readonly string[]): Promise<readonly any[]> => {
+export const getFileDecorations = async (uris: readonly string[]): Promise<readonly any[]> => {
   const repository = await Repositories.getCurrent()
+  await GitRepositoriesRequests.execute({
+    id: 'decorations',
+    fn: GitRequests.getDecorations,
+    args: {
+      cwd: repository.path,
+      gitPath: repository.gitPath,
+      exec: Git.exec,
+      uris,
+    },
+  })
   // TODO
   return []
 }
