e78a2d32deb260c3edfccdea205902b9c6765486
Le Vivilet <levivilet@pm.me>
2025-11-23 01:21:53 +0100

    feature: when discarding untracked file, remove it using file system (#367)

--- packages/git-requests/src/parts/GitRequestsDiscard/GitRequestsDiscard.js
+++ packages/git-requests/src/parts/GitRequestsDiscard/GitRequestsDiscard.js
@@ -1,9 +1,26 @@
 import { GitError } from '../GitError/GitError.js'
+import { getModifiedFiles } from '../GitRequestsGetModifiedFiles/GitRequestsGetModifiedFiles.js'
+import * as FileStateType from '../FileStateType/FileStateType.js'
 
+const partitionFiles = (files, untracked) => {
+  const toDelete = []
+  const toRestore = []
+  for (const file of files) {
+    if (untracked.includes(file)) {
+      toDelete.push(file)
+    } else {
+      toRestore.push(file)
+    }
+  }
+  return {
+    toDelete,
+    toRestore,
+  }
+}
 /**
- * @param {{cwd:string,gitPath:string, file:string, exec:any, confirm:any }} options
+ * @param {{cwd:string,gitPath:string, file:string, exec:any, confirm:any, remove:any  }} options
  */
-export const discard = async ({ cwd, gitPath, file, exec, confirm }) => {
+export const discard = async ({ cwd, gitPath, file, exec, confirm, remove }) => {
   try {
     const confirmResult = await confirm({
       message: `Are you sure you want to discard ${file}`,
@@ -11,12 +28,29 @@ export const discard = async ({ cwd, gitPath, file, exec, confirm }) => {
     if (!confirmResult) {
       return
     }
-    const gitResult = await exec({
-      args: ['restore', '--', file],
+    const status = await getModifiedFiles({
       cwd,
       gitPath,
-      name: 'discard',
+      exec,
     })
+    const untracked = status.index.filter((item) => item.status === FileStateType.Untracked).map((item) => item.file)
+
+    const { toDelete, toRestore } = partitionFiles([file], untracked)
+
+    if (toDelete.length > 0) {
+      for (const item of toDelete) {
+        await remove(`${cwd}/${item}`)
+      }
+    }
+
+    for (const item of toRestore) {
+      const gitResult = await exec({
+        args: ['restore', '--', item],
+        cwd,
+        gitPath,
+        name: 'discard',
+      })
+    }
   } catch (error) {
     throw new GitError(error, 'discard')
   }
