8be7f09a85b58d886b12518bf6555c5302878c0f
Le Vivilet <levivilet@pm.me>
2025-12-09 15:05:09 +0100

    feature: add name to selectBranch status bar item

--- packages/extension/src/parts/ExtensionHostCommand/ExtensionHostCommandSelectBranch.js
+++ packages/extension/src/parts/ExtensionHostCommand/ExtensionHostCommandSelectBranch.js
@@ -3,6 +3,7 @@ import * as CommandId from '../CommandId/CommandId.js'
 export const id = CommandId.GitSelectBranch
 
 export const execute = () => {
+  // console.log('select branch')
   // TODO
   // 1. get all branch names
   // 2. show the quickpick with those branch names
