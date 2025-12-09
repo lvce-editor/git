a285c5e8fe75ff19122196f76ca3844e3fda5e0f
Le Vivilet <levivilet@pm.me>
2025-12-09 15:17:06 +0100

    feature: add mock branch picker (#394)

--- packages/extension/src/parts/ExtensionHostCommand/ExtensionHostCommandSelectBranch.js
+++ packages/extension/src/parts/ExtensionHostCommand/ExtensionHostCommandSelectBranch.js
@@ -2,8 +2,11 @@ import * as CommandId from '../CommandId/CommandId.js'
 
 export const id = CommandId.GitSelectBranch
 
-export const execute = () => {
-  // console.log('select branch')
+export const execute = async () => {
+  const branchNames = ['a', 'b', 'c']
+
+  // @ts-ignore
+  await vscode.confirm('Select Branch')
   // TODO
   // 1. get all branch names
   // 2. show the quickpick with those branch names
