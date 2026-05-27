eb1f3246ef8cba55f8b867a0b086f3fc5d5784fc
Le Vivilet <levivilet@pm.me>
2026-05-27 15:29:29 +0200

    feature: add git.init test implementation

---
+++ packages/e2e/src/git-init.ts
@@ -0,0 +1,25 @@
+import type { Test } from '@lvce-editor/test-with-playwright'
+
+export const name = 'git.init'
+
+export const skip = true
+
+// @ts-ignore
+export const test: Test = async ({ Command, expect, FileSystem, Git, KeyBoard, Locator, Settings, SideBar, SourceControl, Workspace }) => {
+  // arrange
+  const tmpDir = await FileSystem.getTmpDir({ scheme: 'file' })
+  await Workspace.setPath(tmpDir)
+
+  // act
+  await Git.init()
+
+  // assert
+  // @ts-ignore
+  await FileSystem.shouldHaveFolder(`${tmpDir}/.git`)
+  await Git.shouldHaveInvocations([
+    {
+      command: ['git', 'init'],
+      cwd: tmpDir,
+    },
+  ])
+}
