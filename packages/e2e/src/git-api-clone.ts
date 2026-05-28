dadf257085445fe46ba920917700bfa44fd7aca2
Le Vivilet <levivilet@pm.me>
2026-05-28 22:06:47 +0200

    refactor: update test setup in git.clone to include settings update and fix tmpDir scheme (#483)

--- packages/e2e/src/git-api-clone.ts
+++ packages/e2e/src/git-api-clone.ts
@@ -2,14 +2,17 @@ import type { Test } from '@lvce-editor/test-with-playwright'
 
 export const name = 'git.clone'
 
-export const skip = 1
+// export const skip = 1
 
-export const test: Test = async ({ Command, FileSystem, Workspace }) => {
+export const test: Test = async ({ Command, FileSystem, Settings, Workspace }) => {
   // arrange
-  const tmpDir = await FileSystem.getTmpDir()
+  const tmpDir = await FileSystem.getTmpDir({ scheme: 'file' })
   const cloneDir = `${tmpDir}/clone`
 
   await Workspace.setPath(tmpDir)
+  await Settings.update({
+    'git.path': '/usr/bin/git',
+  })
   const setupFixtureUrl = import.meta.resolve('../fixtures/git-api-clone')
   await Command.execute('ExtensionHost.executeCommand', 'git.loadFixture', setupFixtureUrl)
 
