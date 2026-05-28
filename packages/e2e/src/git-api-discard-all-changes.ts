fb6312aa700802fda504a61aa2d99d6d82174955
Le Vivilet <levivilet@pm.me>
2026-05-28 19:05:53 +0200

    refactor: comment out skip export in git.discard-all-changes test

--- packages/e2e/src/git-api-discard-all-changes.ts
+++ packages/e2e/src/git-api-discard-all-changes.ts
@@ -2,7 +2,7 @@ import type { Test } from '@lvce-editor/test-with-playwright'
 
 export const name = 'git.discard-all-changes'
 
-export const skip = 1
+// export const skip = 1
 
 export const test: Test = async ({ Command, FileSystem, Git, Workspace }) => {
   // arrange
