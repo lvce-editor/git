7ef424eb2f6bc0fe14eb506452212219cde941c8
Le Vivilet <levivilet@pm.me>
2026-05-28 17:46:01 +0200

    refactor: remove skip export from git checkout test

--- packages/e2e/src/git-api-checkout.ts
+++ packages/e2e/src/git-api-checkout.ts
@@ -2,8 +2,6 @@ import type { Test } from '@lvce-editor/test-with-playwright'
 
 export const name = 'git.checkout'
 
-export const skip = 1
-
 export const test: Test = async ({ Command, FileSystem, Git, Workspace }) => {
   // arrange
   const tmpDir = await FileSystem.getTmpDir({ scheme: 'file' })
