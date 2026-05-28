24607fbaffb8b91c32f6fc9d567a7c551669b8dc
Le Vivilet <levivilet@pm.me>
2026-05-29 00:06:45 +0200

    fix

--- packages/e2e/src/git-api-sync-rebase.ts
+++ packages/e2e/src/git-api-sync-rebase.ts
@@ -2,6 +2,8 @@ import type { Test } from '@lvce-editor/test-with-playwright'
 
 export const name = 'git.sync-rebase'
 
+export const skip = 1
+
 export const test: Test = async ({ Command, FileSystem, Git, Workspace }) => {
   // arrange
   const tmpDir = await FileSystem.getTmpDir({ scheme: 'file' })
