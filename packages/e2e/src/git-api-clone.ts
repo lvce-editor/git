aaa8e47518a27fa693d1372eda0239622bd33b7f
Le Vivilet <levivilet@pm.me>
2026-05-28 18:39:50 +0200

    e

--- packages/e2e/src/git-api-clone.ts
+++ packages/e2e/src/git-api-clone.ts
@@ -2,7 +2,7 @@ import type { Test } from '@lvce-editor/test-with-playwright'
 
 export const name = 'git.clone'
 
-// export const skip = 1
+export const skip = 1
 
 export const test: Test = async ({ Command, FileSystem, Workspace }) => {
   // arrange
