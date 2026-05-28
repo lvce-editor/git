499a7b01a2ea59193e34a51e68b22034a9c21141
Le Vivilet <levivilet@pm.me>
2026-05-28 18:20:21 +0200

    feature: add test for git.deleteTag functionality

---
+++ packages/e2e/src/git-api-delete-tag.ts
@@ -0,0 +1,34 @@
+import type { Test } from '@lvce-editor/test-with-playwright'
+
+export const name = 'git.deleteTag'
+
+export const skip = 1
+
+export const test: Test = async ({ Command, FileSystem, Git, Workspace }) => {
+  // arrange
+  const tmpDir = await FileSystem.getTmpDir({ scheme: 'file' })
+  const workspaceDir = `${tmpDir}/workspace`
+  const tagName = 'v0.1'
+
+  await Workspace.setPath(tmpDir)
+  const fixtureUrl = import.meta.resolve('../fixtures/git-api-delete-tag')
+  await Command.execute('ExtensionHost.executeCommand', 'git.loadFixture', fixtureUrl)
+  await Workspace.setPath(workspaceDir)
+
+  // act
+  await Command.execute('ExtensionHost.executeCommand', 'git.deleteTag', tagName)
+
+  // assert
+  try {
+    await FileSystem.readFile(`${workspaceDir}/.git/refs/tags/${tagName}`)
+    throw new Error(`expected tag ${tagName} to be deleted`)
+  } catch {
+    // expected: reading the deleted tag ref should fail
+  }
+  await Git.shouldHaveInvocations([
+    {
+      command: ['git', 'tag', '-d', tagName],
+      cwd: workspaceDir,
+    },
+  ])
+}
