e9bc2874a684080b375d90b9a17ededa7821111b
Le Vivilet <levivilet@pm.me>
2026-05-28 18:53:55 +0200

    skip

--- packages/e2e/src/git-api-init-bare.ts
+++ packages/e2e/src/git-api-init-bare.ts
@@ -2,6 +2,8 @@ import type { Test } from '@lvce-editor/test-with-playwright'
 
 export const name = 'git.init-bare'
 
+export const skip = 1
+
 export const test: Test = async ({ FileSystem, Git, Workspace }) => {
   // arrange
   const tmpDir = await FileSystem.getTmpDir({ scheme: 'file' })
