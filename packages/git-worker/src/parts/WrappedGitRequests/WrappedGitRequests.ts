3abc844d39bff1f8b2db0295aed9a907ba2bf3ec
Le Vivilet <levivilet@pm.me>
2026-05-28 14:49:43 +0200

    fix

--- packages/git-worker/src/parts/WrappedGitRequests/WrappedGitRequests.ts
+++ packages/git-worker/src/parts/WrappedGitRequests/WrappedGitRequests.ts
@@ -1,12 +1,13 @@
 import * as GitRequests from '../GitRequests/GitRequests.ts'
 import * as WrapGitCommand from '../WrapGitCommand/WrapGitCommand.ts'
 
-const gitRequestEntries = Object.entries(GitRequests).filter((entry) => typeof entry[1] === 'function') as unknown as ReadonlyArray<
-  readonly [string, (args: Readonly<Record<string, any>>) => Promise<any>]
->
+const gitRequestEntries = Object.entries(GitRequests).filter(
+  (entry: readonly [string, unknown]) => typeof entry[1] === 'function',
+) as unknown as ReadonlyArray<readonly [string, (args: { readonly [key: string]: unknown }) => Promise<unknown>]>
 
 export const wrappedGitRequests = Object.fromEntries(
-  gitRequestEntries.map(([key, value]) => {
+  gitRequestEntries.map((entry: readonly [string, (args: { readonly [key: string]: unknown }) => Promise<unknown>]) => {
+    const [key, value] = entry
     return [key, WrapGitCommand.wrapGitCommand(value)]
   }),
 )
