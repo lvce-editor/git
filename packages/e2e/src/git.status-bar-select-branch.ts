05daaf85f21ea72b4960f094f8ce7a38ab203ec3
Le Vivilet <levivilet@pm.me>
2026-06-28 21:02:08 +0200

    fix

--- packages/e2e/src/git.status-bar-select-branch.ts
+++ packages/e2e/src/git.status-bar-select-branch.ts
@@ -6,19 +6,16 @@ export const skip = 1
 
 export const test: Test = async ({ Command, expect, FileSystem, Locator, SideBar, Workspace }) => {
   // arrange
-  const tmpDir = await FileSystem.getTmpDir({ scheme: 'file' })
-  const workspaceDir = `${tmpDir}/workspace`
-
-  await Workspace.setPath(tmpDir)
-  const fixtureUrl = import.meta.resolve('../fixtures/git-api-checkout')
+  // const tmpDir = await FileSystem.getTmpDir({ scheme: 'file' })
+  // const workspaceDir = `${tmpDir}/workspace`
+  // await Workspace.setPath(tmpDir)
+  // const fixtureUrl = import.meta.resolve('../fixtures/git-api-checkout')
   // TODO
   // await Command.execute('ExtensionHost.executeCommand', 'git.loadFixture', fixtureUrl)
   // await Workspace.setPath(workspaceDir)
   // await SideBar.open('Source Control')
-
   // // act
   // await Locator('.StatusBarItem[data-name="git.selectBranch"]').click()
-
   // // assert
   // await expect(Locator('#QuickPick')).toBeVisible()
   // await expect(Locator('text=feature')).toBeVisible()
