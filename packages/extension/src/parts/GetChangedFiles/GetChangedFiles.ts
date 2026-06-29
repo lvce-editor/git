897e65b5fca5cd80ae6ccf7560559210ed8fca6a
Le Vivilet <levivilet@pm.me>
2026-06-29 15:53:27 +0200

    tsc

--- packages/extension/src/parts/GetChangedFiles/GetChangedFiles.ts
+++ packages/extension/src/parts/GetChangedFiles/GetChangedFiles.ts
@@ -2,7 +2,7 @@ import * as Assert from '../Assert/Assert.ts'
 import * as GitWorker from '../GitWorker/GitWorker.ts'
 import * as GitWorkerCommandType from '../GitWorkerCommandType/GitWorkerCommandType.ts'
 
-export const getChangedFiles = async (cwd) => {
+export const getChangedFiles = async () => {
   const result = await GitWorker.invoke(GitWorkerCommandType.GitGetChangedFiles)
   Assert.array(result)
   return result
