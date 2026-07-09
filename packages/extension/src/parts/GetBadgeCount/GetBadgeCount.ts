acd5df53c0f42e5d4435dbb859ade483467a0152
Le Vivilet <levivilet@pm.me>
2026-07-09 14:07:13 +0000

    fix: report git source control badge count

--- packages/extension/src/parts/GetBadgeCount/GetBadgeCount.ts
+++ packages/extension/src/parts/GetBadgeCount/GetBadgeCount.ts
@@ -1,3 +1,17 @@
-export const getBadgeCount = async (cwd) => {
-  return 0
+import * as GitWorker from '../GitWorker/GitWorker.ts'
+import * as GitWorkerCommandType from '../GitWorkerCommandType/GitWorkerCommandType.ts'
+
+export const getBadgeCount = async (cwd): Promise<number> => {
+  const groups = await GitWorker.invoke(GitWorkerCommandType.GitGetGroups, {})
+  if (!Array.isArray(groups)) {
+    return 0
+  }
+  let badgeCount = 0
+  for (const group of groups) {
+    if (!group || !Array.isArray(group.items)) {
+      continue
+    }
+    badgeCount += group.items.length
+  }
+  return badgeCount
 }
