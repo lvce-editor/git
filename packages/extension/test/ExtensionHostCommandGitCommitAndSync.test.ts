25322cf9a678ac6600b47d6de4714199c2acb6c4
Le Vivilet <levivilet@pm.me>
2026-07-09 16:13:29 +0200

    feature: contribute commit sync button (#527)

---
+++ packages/extension/test/ExtensionHostCommandGitCommitAndSync.test.ts
@@ -0,0 +1,14 @@
+import { test, expect, jest } from '@jest/globals'
+import * as ExtensionHostCommandGitCommitAndSync from '../src/parts/ExtensionHostCommand/ExtensionHostCommandGitCommitAndSync.ts'
+import * as GitWorker from '../src/parts/GitWorker/GitWorker.ts'
+import * as GitWorkerCommandType from '../src/parts/GitWorkerCommandType/GitWorkerCommandType.ts'
+
+test('execute', async () => {
+  const invoke = jest.spyOn(GitWorker, 'invoke').mockImplementation(async () => {})
+
+  await ExtensionHostCommandGitCommitAndSync.execute('test message')
+
+  expect(invoke).toHaveBeenCalledTimes(2)
+  expect(invoke).toHaveBeenNthCalledWith(1, GitWorkerCommandType.GitCommit, { message: 'test message' })
+  expect(invoke).toHaveBeenNthCalledWith(2, 'Command.gitSync')
+})
