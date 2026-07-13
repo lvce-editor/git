21e316da1be0cdbce710b73a787f052c8856d5d9
Le Vivilet <levivilet@pm.me>
2026-07-13 15:50:27 +0000

    fix: use current status bar selector

--- packages/e2e/src/git.status-bar-select-branch.ts
+++ packages/e2e/src/git.status-bar-select-branch.ts
@@ -24,7 +24,7 @@ export const test: Test = async ({ Command, expect, FileSystem, Locator, QuickPi
   await Workspace.setPath(workspaceDir)
   await SideBar.open('Source Control')
 
-  const branchStatusBarItem = Locator('.StatusBarItem[data-name="git.showBranchPicker"]')
+  const branchStatusBarItem = Locator('.StatusBarItem[name="git.showBranchPicker"]')
   await expect(branchStatusBarItem).toBeVisible()
   await expect(branchStatusBarItem).toHaveText('main')
 
