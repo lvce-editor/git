37982e4d30398849b4e4518816d9372265cf6389
Le Vivilet <levivilet@pm.me>
2026-07-28 12:13:13 +0000

    fix: describe git sync status accessibly

--- packages/git-requests/src/parts/GitRequestsGetUpstreamChanges/GitRequestsGetUpstreamChanges.ts
+++ packages/git-requests/src/parts/GitRequestsGetUpstreamChanges/GitRequestsGetUpstreamChanges.ts
@@ -4,20 +4,28 @@ import { GitError } from '../GitError/GitError.ts'
 export interface GitUpstreamChanges {
   readonly incoming: number
   readonly outgoing: number
+  readonly upstream: string
 }
 
 export const getUpstreamChanges = async ({ cwd, exec, gitPath }: GitRequestContext): Promise<GitUpstreamChanges> => {
   try {
-    const gitResult = await exec({
+    const changesResult = await exec({
       args: ['rev-list', '--left-right', '--count', 'HEAD...@{upstream}'],
       cwd,
       gitPath,
       name: 'getUpstreamChanges',
     })
-    const [outgoing = 0, incoming = 0] = gitResult.stdout.trim().split(/\s+/).map(Number)
+    const upstreamResult = await exec({
+      args: ['rev-parse', '--abbrev-ref', '--symbolic-full-name', '@{upstream}'],
+      cwd,
+      gitPath,
+      name: 'getUpstreamChanges',
+    })
+    const [outgoing = 0, incoming = 0] = changesResult.stdout.trim().split(/\s+/).map(Number)
     return {
       incoming,
       outgoing,
+      upstream: upstreamResult.stdout.trim(),
     }
   } catch (error) {
     throw new GitError(error, 'getUpstreamChanges')
--- packages/git-requests/test/GitRequestsGetUpstreamChanges.test.ts
+++ packages/git-requests/test/GitRequestsGetUpstreamChanges.test.ts
@@ -3,10 +3,18 @@ import type { GitExec } from '../src/parts/Types/Types.ts'
 import * as GitRequestsGetUpstreamChanges from '../src/parts/GitRequestsGetUpstreamChanges/GitRequestsGetUpstreamChanges.ts'
 
 test('getUpstreamChanges', async (): Promise<void> => {
-  const exec = jest.fn<GitExec>(async () => ({
-    stderr: '',
-    stdout: '2\t3\n',
-  }))
+  const exec = jest.fn<GitExec>(async ({ args }) => {
+    if (args[0] === 'rev-list') {
+      return {
+        stderr: '',
+        stdout: '2\t3\n',
+      }
+    }
+    return {
+      stderr: '',
+      stdout: 'origin/main\n',
+    }
+  })
 
   await expect(
     GitRequestsGetUpstreamChanges.getUpstreamChanges({
@@ -17,9 +25,16 @@ test('getUpstreamChanges', async (): Promise<void> => {
   ).resolves.toEqual({
     incoming: 3,
     outgoing: 2,
+    upstream: 'origin/main',
   })
-  expect(exec).toHaveBeenCalledWith({
+  expect(exec).toHaveBeenNthCalledWith(1, {
     args: ['rev-list', '--left-right', '--count', 'HEAD...@{upstream}'],
+    cwd: '/test/workspace',
+    gitPath: '/test/git',
+    name: 'getUpstreamChanges',
+  })
+  expect(exec).toHaveBeenNthCalledWith(2, {
+    args: ['rev-parse', '--abbrev-ref', '--symbolic-full-name', '@{upstream}'],
     cwd: '/test/workspace',
     gitPath: '/test/git',
     name: 'getUpstreamChanges',
