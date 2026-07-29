37fd38c031b6e7d9987c66a1083a998bcb93b9aa
Le Vivilet <levivilet@pm.me>
2026-07-29 14:03:49 +0200

    test: enable git branch picker icon coverage (#563)

--- packages/e2e/src/git.branch-picker-local-icon.ts
+++ packages/e2e/src/git.branch-picker-local-icon.ts
@@ -2,8 +2,6 @@ import type { Test } from '@lvce-editor/test-with-playwright'
 
 export const name = 'git.branch-picker-local-icon'
 
-export const skip = 1
-
 export const test: Test = async ({ Command, expect, FileSystem, Locator, QuickPick, SideBar, Workspace }) => {
   // arrange
   const tmpDir = await FileSystem.getTmpDir({ scheme: 'file' })
