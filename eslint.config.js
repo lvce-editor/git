de6025a404177dfd01d07cb8ec054e127df1f25d
Le Vivilet <levivilet@pm.me>
2026-08-01 11:35:14 -0700

    feature: update eslint dependencies (#567)

--- eslint.config.js
+++ eslint.config.js
@@ -1,6 +1,7 @@
+import { defineConfig } from 'eslint/config'
 import * as config from '@lvce-editor/eslint-config'
 
-export default [
+export default defineConfig([
   {
     ignores: ['packages/git-web/**'],
   },
@@ -43,4 +44,4 @@ export default [
       'unicorn/prefer-number-properties': 'off',
     },
   },
-]
+])
